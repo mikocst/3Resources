248bde50c78606edfb061151f5cd4ca8164747cb
mikocst <mikocastillo7@gmail.com>
2024-10-07 23:24:44 -0500

    adjusted 3D scene in about page and compiled shaders for about 3D Scene

--- src/shaders/vertexA.glsl
+++ src/shaders/vertexA.glsl
@@ -1,7 +1,16 @@
+uniform float uTime;
 varying vec2 vUv;
 
 void main()
 {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    vUv = uv;
+    vec3 pos = position;
+
+    
+    pos.x += tan(pos.x * 0.2 + uTime) * 0.000002;
+    pos.z += sin(pos.y * 1.5 * uTime) * 1.1;
+
+   
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+
+    vUv = uv; 
 }
